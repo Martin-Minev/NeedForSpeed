46f2ab97906f4fc4137d95263841bf43889ecbad
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2021-03-09 23:16:17 +0100

    1st

---
+++ Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        private const double DefaultFuelConsumption = 3;
+
+        public Car(int horsePower, double fuel)
+            : base (horsePower, fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+
+        public override double FuelConsumption => DefaultFuelConsumption;
+        
+    }
+}
